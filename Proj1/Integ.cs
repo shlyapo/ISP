c2d0d63a86ae11272b896ed7eb1c5d4795722ce0
shlyapo <72437851+shlyapo@users.noreply.github.com>
2021-12-12 12:47:39 +0300

    Add files via upload

---
+++ Proj1/Integ.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Proj1
+{
+    public class Int
+    {
+        public delegate void result(double res);
+        public event result Result;
+
+        public void Integ()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            double step = 0.00000001;
+            double sum = 0;
+            double y;
+            for (double x = step / 2; x < 1; x += step)
+            {
+                y = Math.Sin(x);
+                sum += y;
+            }
+            sum *= step;
+            Result?.Invoke(sum);
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed);
+
+        }
+        public void InfRes(double result)
+        {
+            Console.WriteLine(result);
+        }
+
+    }
+}
